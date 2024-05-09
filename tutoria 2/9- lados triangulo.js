073fd92f07454cd1039c53f457919195ecd62a1d
ecker91 <165970652+ecker91@users.noreply.github.com>
2024-05-08 21:26:38 -0300

    Update 9- lados triangulo.js

--- tutoria 2/9- lados triangulo.js
+++ tutoria 2/9- lados triangulo.js
@@ -1,6 +1,6 @@
 //9) Elabore um programa que leia três lados de um triângulo e mostre na tela se ele é equilátero, isósceles ou escaleno. Quando os três lados forem iguais trata-se de um triângulo equilátero, dois lados iguais é um triângulo isósceles e os três lados diferentes é um triângulo escaleno.
 
-let ladoUm, ladoDois, ladoTres, tipoTriangulo
+let ladoUm, ladoDois, ladoTres
 
 ladoUm = Number(prompt('Digite quantas unidades de medida tem o primeiro lado: '))
 ladoDois = Number(prompt('Digite quantas unidades de medida tem o segundo lado: '))
