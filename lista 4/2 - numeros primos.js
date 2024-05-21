5f9602a41015d6eafc5a62897dcb61b74830be59
ecker91 <165970652+ecker91@users.noreply.github.com>
2024-05-21 19:55:17 -0300

    atualizaçao de 2 - numeros primos.js

--- lista 4/2 - numeros primos.js
+++ lista 4/2 - numeros primos.js
@@ -1,82 +1,20 @@
 //2) Os números primos possuem várias aplicações dentro da Computação, por exemplo, na criptografia. Um número primo é aquele que é divisível apenas por um e por ele mesmo. Faça um programa que peça ao usuário para digitar cinco números inteiros e mostre na tela se são primos ou não.
 
-let numeroUm, numeroDois, numeroTres, numeroQuatro, numeroCinco, contador = 2
+let numero, contador = 2, qtdNumeros = 0
 
-numeroUm = parseInt(prompt('Digite o primeiro número: '))
-numeroDois = parseInt(prompt('Digite o segundo número: '))
-numeroTres = parseInt(prompt('Digite o terceiro número: '))
-numeroQuatro = parseInt(prompt('Digite o quarto número: '))
-numeroCinco = parseInt(prompt('Digite o quinto número: '))
-
-do{
-    if(numeroUm%contador == 0){
-        break
-    }
-    contador++
-}while (contador < numeroUm)
-
-if(contador == numeroUm){
-    alert('O número '+numeroUm+' é primo.')
+while(qtdNumeros <5){
+    numero = parseInt(prompt('Digite um número: '))
+    do{
+        if(numero%contador == 0){
+            break
+        }
+         contador++
+    }while (contador < numero)
+if(contador == numero){
+    alert('O número '+numero+' é primo.')
 }else{
-    alert('O número '+numeroUm+' não é primo.')
+    alert('O número '+numero+' não é primo.')
 }
-
+qtdNumeros++
 contador = 2 
-
-do{
-    if(numeroDois%contador == 0){
-        break
-    }
-    contador++
-}while (contador < numeroDois)
-
-if(contador == numeroDois){
-    alert('O número '+numeroDois+' é primo.')
-}else{
-    alert('O número '+numeroDois+' não é primo.')
-}
-
-contador = 2 
-
-do{
-    if(numeroTres%contador == 0){
-        break
-    }
-    contador++
-}while (contador < numeroTres)
-
-if(contador == numeroTres){
-    alert('O número '+numeroTres+' é primo.')
-}else{
-    alert('O número '+numeroTres+' não é primo.')
-}
-
-contador = 2 
-
-do{
-    if(numeroQuatro%contador == 0){
-        break
-    }
-    contador++
-}while (contador < numeroQuatro)
-
-if(contador == numeroQuatro){
-    alert('O número '+numeroQuatro+' é primo.')
-}else{
-    alert('O número '+numeroQuatro+' não é primo.')
-}
-
-contador = 2 
-
-do{
-    if(numeroCinco%contador == 0){
-        break
-    }
-    contador++
-}while (contador < numeroCinco)
-
-if(contador == numeroCinco){
-    alert('O número '+numeroCinco+' é primo.')
-}else{
-    alert('O número '+numeroCinco+' não é primo.')
 }
