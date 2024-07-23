deeb27b934f82e64399e1a1e4e71e6876760d966
ecker91 <165970652+ecker91@users.noreply.github.com>
2024-07-23 20:45:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function ex1(){
     window.location.href="exercicio 11-06-24/index.html"
 }
+function ex2(){
+    window.location.href="exercicio 02-07-24/index.html"
+}
 function serie(){
     window.location.href="serie harmonica/index.html"
 }
